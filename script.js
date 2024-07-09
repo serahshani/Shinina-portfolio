0af71d52a1eb73089aa4ad5dcdc85226cdc39d0f
serah shani <serahshani035@gmail.com>
2024-07-09 15:34:27 +0300

    edited

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Activist", "Program manager", "Program Dveloper"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
